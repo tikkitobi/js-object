f841cfacf5c26ab34a42a197fde754a65c298b62
Ваше Имя <you@example.com>
2022-07-24 17:04:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// for(let i = 1; i<= 10; i++) {
+//     let name = prompt('Введите имя:');
+//     let age = prompt('Введите возраст:');
+//     let id = i;
+//     console.log('Пользователь - ' + id);
+//     console.log('Имя - ' + name);
+//     console.log('Возраст - ' + age);
+// }
+for(let i = 1; i <= 10; i++){
+
+
+const user = {
+    name: prompt('Введите имя:'),
+    age: prompt('Введите возраст:')
+}
+    let id = i;
+    console.log('Пользователь - ' + id);
+    console.log('Имя - ' + user.name);
+    console.log('Возраст - ' + user.age);
+
+}
